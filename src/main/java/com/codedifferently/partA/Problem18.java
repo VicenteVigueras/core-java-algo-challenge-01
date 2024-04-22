e971e228fda49abcad427ce8d97a0e961b2e891c
Vicente Vigueras <vicente.viguerassilva@gmail.com>
2024-04-22 17:03:56 -0400

    feat: completed problem 18, 19

--- src/main/java/com/codedifferently/partA/Problem18.java
+++ src/main/java/com/codedifferently/partA/Problem18.java
@@ -4,7 +4,11 @@ public class Problem18 {
     public Boolean loneTeen(int a, int b) {
         if((a >= 13 && a <= 19) || !(b >= 13 && b <= 19) || !(a >= 13 && a <= 19) || (b >= 13 && b <= 19)) {
             if(!(a > 19 && b > 19)) {
-                return true;
+                if(!(a==b)) {
+                    if(!((a >= 13 && a <= 19) && (b >= 13 && b <= 19))) {
+                        return true;
+                    }
+                }
             }
          }
         return false;
--- src/main/java/com/codedifferently/partA/Problem19.java
+++ src/main/java/com/codedifferently/partA/Problem19.java
@@ -1,8 +1,22 @@
 package com.codedifferently.partA;
 
 public class Problem19 {
+    public static void main(String[] args) {
+        Problem19 problem19 = new Problem19();
+        problem19.delDel("");
+        // should print ice
+    }
+
     public String delDel(String str) {
-        // your code here
-       return null;
+       if(str.isEmpty()) {
+        return "";
+       } 
+       StringBuilder stringBuilder = new StringBuilder(str);
+       String del = stringBuilder.substring(1, 4);
+        if (del.contentEquals("del") == true) {
+        return stringBuilder.delete(1,4).toString(); 
+    }
+       return str;
+
     }
 }
